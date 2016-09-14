3e55b262b4e8ea52726ce4bd02c24dab33aa4dd3
Chris Cooke <chrooke@geekido.org>
2016-09-13 21:28:54 -0400

    First version

---
+++ clear_matrix_and_blink13/clear_matrix_and_blink13.ino
@@ -0,0 +1,26 @@
+#include <Adafruit_GFX.h>
+#include <Adafruit_NeoMatrix.h>
+#include <Adafruit_NeoPixel.h>
+#ifndef PSTR
+ #define PSTR // Make Arduino Due happy
+#endif
+
+Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(4, 8, 6,
+  NEO_MATRIX_TOP     + NEO_MATRIX_RIGHT +
+  NEO_MATRIX_COLUMNS + NEO_MATRIX_PROGRESSIVE,
+  NEO_GRB            + NEO_KHZ800);
+
+void setup() {
+  pinMode(13, OUTPUT);
+  matrix.begin();
+  matrix.setBrightness(10);
+  matrix.fillScreen(0);
+  matrix.show();
+}
+
+void loop() {
+  digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);              // wait for a second
+  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);              // wait for a second
+}
