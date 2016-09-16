53d1b025b43b093bee362b8de0e364dac133e594
Chris Cooke <chrooke@geekido.org>
2016-09-16 00:06:42 -0400

    Basic animation program. Three different animations, 5 different transitions.

--- animator/animator.ino
+++ animator/animator.ino
@@ -1,5 +1,5 @@
 //Comment out to make the program silent
-#define AUDIO
+//#define AUDIO
 
 //Remove comment to get debug information to Serial
 //#define DEBUG 
@@ -13,17 +13,15 @@
 #include <Timer.h>
 
 #define MATRIX_PIN 6
-#define SONAR_PIN 5
 #define SPEAKER_PIN 10
-#define LRS_TRANSITION 10
-#define SRS_TRANSITION 10
-#define AD_TRANSITION 15
+#define MAX_WALKERS 10
+#define NUM_STATES 3
+#define TIME_PER_STATE 30000L
 
 enum State {
-  LONG_RANGE_SCAN,
-  SHORT_RANGE_SCAN,
-  AWESOME_DETECTED,
-  CLEAR
+  WALKERS,
+  FLASHY,
+  HIVE
 };
 
 Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(4, 8, MATRIX_PIN,
@@ -35,15 +33,12 @@ Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(4, 8, MATRIX_PIN,
 uint16_t w    = matrix.width();
 uint16_t h    = matrix.height();
 State current_state;
-boolean detect;
-uint16_t range;
 Timer t;
 uint8_t scan_event;
-int16_t upgrade_transition_count;
-int16_t downgrade_transition_count;
 uint16_t state_delay;
 //holds the "walkers" for the long range scanner.
-int16_t lrs_walkers[SRS_TRANSITION-1][6]; 
+int16_t lrs_walkers[MAX_WALKERS][6];
+uint8_t num_walkers; 
 
 
 void setup() {
@@ -51,52 +46,19 @@ void setup() {
   Serial.begin(9600);
 #endif
   randomSeed(analogRead(0)-analogRead(1)+analogRead(2)-analogRead(3)+analogRead(4));
-  pinMode(SONAR_PIN,INPUT);
   pinMode(SPEAKER_PIN,OUTPUT);
   matrix.begin();
   matrix.setBrightness(10);
-  matrix.setTextWrap(false);
-  matrix.setTextColor(matrix.Color(255,0,0));
-  delay(500); //allow sonar to calibrate and get first reading
-  for (uint16_t i; i<SRS_TRANSITION-1;i++) {
-    lrs_walkers[i][0]=random(w);
-    lrs_walkers[i][1]=random(h);
-    lrs_walkers[i][2]=random(8);
-    lrs_walkers[i][3]=random(1,255);    
-    lrs_walkers[i][4]=random(1,255); 
-    lrs_walkers[i][5]=random(1,255);    
-  }
-  startLongRangeScan();
+  scan_event=t.every(TIME_PER_STATE,changeState);
+  changeState();
 }
 
 void loop() {
   switch (current_state) {
-    case LONG_RANGE_SCAN:
-        if (detect) {
-            if (range>90) {
-              upgrade_transition_count=0;
-            } else if (range>60) {
-              upgrade_transition_count--;
-              upgrade_transition_count=max(0,upgrade_transition_count);
-            } else if (range>36) {
-              upgrade_transition_count++;
-            } else if (range>12) {
-              upgrade_transition_count+=2;
-            }
-            if (range>12) {
-#ifdef DEBUG
-              Serial.print("Range: ");Serial.println(range);
-              Serial.print("upgrade_transition_count: ");Serial.println(upgrade_transition_count);
-#endif
-            }
-            detect=false;                  
-         }
-         if (upgrade_transition_count >= SRS_TRANSITION) {
-            startShortRangeScan();
-         }
+    case WALKERS:
         //move walkers
         matrix.fillScreen(0);
-        for (uint16_t i=0;i<=upgrade_transition_count;i++){
+        for (uint16_t i=0;i<=num_walkers;i++){
           matrix.drawPixel(lrs_walkers[i][0],
                            lrs_walkers[i][1],
                            matrix.Color(
@@ -106,134 +68,73 @@ void loop() {
           moveWalker(i);         
         }
         matrix.show();
-        state_delay=750-(700/SRS_TRANSITION)*upgrade_transition_count;
-        delay(state_delay);
         break;
     
-    case SHORT_RANGE_SCAN:
-        if (detect) {
-            if (range>40) {
-              downgrade_transition_count++;
-              upgrade_transition_count=0;
-            } else if (range>30) {
-              upgrade_transition_count--;
-              upgrade_transition_count=max(0,upgrade_transition_count);
-            } else if (range>20) {
-              downgrade_transition_count--;
-              downgrade_transition_count=max(0,downgrade_transition_count);
-              upgrade_transition_count++;
-            } else if (range>12) {
-              downgrade_transition_count-=2;
-              downgrade_transition_count=max(0,downgrade_transition_count);
-              upgrade_transition_count++;
-            }
-            if (range>12) {
-#ifdef DEBUG
-              Serial.print("Range: ");Serial.println(range);
-              Serial.print("downgrade_transition_count: ");Serial.println(downgrade_transition_count);
-              Serial.print("upgrade_transition_count: ");Serial.println(upgrade_transition_count);
-#endif
-            }
-            detect=false;
-         }
-        if (downgrade_transition_count >= LRS_TRANSITION) {
-            startLongRangeScan();
-        }
-        if (upgrade_transition_count >= AD_TRANSITION) {
-            AwesomeDetected();
-        }
-        matrix.drawPixel(random(w),random(h),matrix.Color(random(255), random(255), random(255)));
+    case FLASHY:
         matrix.drawPixel(random(w),random(h),matrix.Color(random(255), random(255), random(255)));
         matrix.show();
-        state_delay=250-(240/AD_TRANSITION)*upgrade_transition_count;
-        delay(state_delay);
-      break;
+        break;
 
-    case AWESOME_DETECTED:
-      matrix.fillScreen(0);
-      matrix.setTextColor(matrix.Color(random(255), random(255), random(255)));
-      matrix.setCursor(random(4)-2, 0);
-      matrix.print(F("!"));
-      matrix.show();
-#ifdef AUDIO
-      playTone(random(75,300),random(5,25));
-#endif
-      delay(100);
-      break;
-
-    case CLEAR:
-      break;
+    case HIVE:
+        //move hive
+        matrix.fillScreen(0);
+        matrix.drawPixel(lrs_walkers[0][0],
+                         lrs_walkers[0][1],
+                         matrix.Color(
+                         lrs_walkers[0][3],
+                         lrs_walkers[0][4],
+                         lrs_walkers[0][5]));
+        moveWalker(0);
+        lrs_walkers[0][2]=random(8);         
+        matrix.show();
+        break;
   }
 
+  delay(state_delay);
   t.update();
 }
 
-void startLongRangeScan() {
+void startWalkers() {
 #ifdef DEBUG
-  Serial.println("Switching to long range scanner...");
+  Serial.println("Switching to walkers...");
 #endif
-  t.stop(scan_event);
-  detect=false;
-  matrix.fillScreen(0);
-  matrix.show();
-  current_state=LONG_RANGE_SCAN;
-  upgrade_transition_count=0;  
-  scan_event=t.every(1000,getRange);
-  Countdown();
+  current_state=WALKERS;
+  num_walkers=random(MAX_WALKERS);
+  state_delay=random(100,750);
+  for (uint16_t i; i<MAX_WALKERS;i++) {
+    lrs_walkers[i][0]=random(w);
+    lrs_walkers[i][1]=random(h);
+    lrs_walkers[i][2]=random(8);
+    lrs_walkers[i][3]=random(1,255);    
+    lrs_walkers[i][4]=random(1,255); 
+    lrs_walkers[i][5]=random(1,255);    
+  }  
+  randomTransition();
 }
 
-void startShortRangeScan() {
+void startFlashy() {
 #ifdef DEBUG
-  Serial.println("Switching to short range scanner...");
+  Serial.println("Switching to flashy...");
 #endif
-  t.stop(scan_event);
-  detect=false;
-  current_state=SHORT_RANGE_SCAN;
-  downgrade_transition_count=0; 
-  upgrade_transition_count=0;
-  scan_event=t.every(250,getRange);
-  matrix.fillScreen(0);
-  for (int i=255;i>=0;i--) {
-    matrix.drawRect(0,0,w,h,matrix.Color(i,i,i));
-    matrix.show();
-  #ifdef AUDIO
-    playTone(255-(i/2)-25,1);
-  #endif
-  }
+  current_state=FLASHY;
+  state_delay=random(250);
+  randomTransition();
 }
 
-void AwesomeDetected() {
+void startHive() {
 #ifdef DEBUG
-  Serial.println("Awesome detected!");
+  Serial.println("Switching to hive...");
 #endif
-  t.stop(scan_event);
-  detect=false;
-  matrix.fillScreen(0);
-  matrix.show();
-  current_state=AWESOME_DETECTED;
-  WipeOut(matrix.Color(0,255,0),100);  
-  WipeOut(0,100);
-  delay(100);
-  scan_event=t.after(5000L,Clear);
-}
-
-void Clear() {
-#ifdef DEBUG
-  Serial.println("Clear...");
-#endif
-  detect=false;
-  WipeOut(matrix.Color(random(255),random(255),random(255)),100);
-  WipeOut(matrix.Color(random(255),random(255),random(255)),75);  
-  WipeOut(0,50);
-  current_state=CLEAR;
-  delay(15000);
-  startLongRangeScan();
-//  scan_event=t.after(15000L,startLongRangeScan);  
-}
-
-void getRange() {
-    range=pulseIn(SONAR_PIN, HIGH)/147;
-    detect=true;
+  current_state=HIVE;
+  num_walkers=1;
+  state_delay=20;
+  lrs_walkers[0][0]=random(w);
+  lrs_walkers[0][1]=random(h);
+  lrs_walkers[0][2]=random(8);
+  lrs_walkers[0][3]=random(1,255);    
+  lrs_walkers[0][4]=random(1,255); 
+  lrs_walkers[0][5]=random(1,255);    
+  randomTransition();
 }
 
 void playTone(int tone, int duration) {
@@ -245,16 +146,21 @@ void playTone(int tone, int duration) {
   }
 }
 
+void changeState() {
+  switch (random(NUM_STATES)) {
+    case WALKERS:
+      startWalkers();
+      break;
+    case FLASHY:
+      startFlashy();
+      break;
+    case HIVE:
+      startHive();
+  }
+}
+
 void moveWalker(uint16_t walker) {
   int16_t cx,cy;
-#ifdef DEBUG
-  Serial.print("++moveWalker pre: ");Serial.print(walker);Serial.print(" ");
-  Serial.print(lrs_walkers[walker][0]);Serial.print(" ");  Serial.print(" ");Serial.print(lrs_walkers[walker][1]);Serial.print(" "); 
-  Serial.print(" ");Serial.print(lrs_walkers[walker][2]);Serial.print(" ");
-  Serial.print(" ");Serial.print(lrs_walkers[walker][3]);Serial.print(" ");
-  Serial.print(" ");Serial.print(lrs_walkers[walker][4]);Serial.print(" ");
-  Serial.print(" ");Serial.print(lrs_walkers[walker][5]);Serial.println(" ");
-#endif
   switch(lrs_walkers[walker][2]) {
     case 0: //up, left
             cy=lrs_walkers[walker][1]-1;
@@ -291,27 +197,57 @@ void moveWalker(uint16_t walker) {
   }
   if (cx<0 || cx >= w || cy<0 || cy >= h) { //collision! Try a different direction.
     lrs_walkers[walker][2]=random(8);
-#ifdef DEBUG
-  Serial.print("++moveWalker collision: ");Serial.print(walker);Serial.print(" ");
-  Serial.print(lrs_walkers[walker][0]);Serial.print(" ");  Serial.print(" ");Serial.print(lrs_walkers[walker][1]);Serial.print(" "); 
-  Serial.print(" ");Serial.print(lrs_walkers[walker][2]);Serial.print(" ");
-  Serial.print(" ");Serial.print(lrs_walkers[walker][3]);Serial.print(" ");
-  Serial.print(" ");Serial.print(lrs_walkers[walker][4]);Serial.print(" ");
-  Serial.print(" ");Serial.print(lrs_walkers[walker][5]);Serial.println(" ");
-#endif
     moveWalker(walker);
   } else { //OK
     lrs_walkers[walker][0]=cx;
     lrs_walkers[walker][1]=cy;
   }
+}
+
+void randomTransition() {
+  switch(random(6)) {
+    case 0:
 #ifdef DEBUG
-  Serial.print("++moveWalker post: ");Serial.print(walker);Serial.print(" ");
-  Serial.print(lrs_walkers[walker][0]);Serial.print(" ");  Serial.print(" ");Serial.print(lrs_walkers[walker][1]);Serial.print(" "); 
-  Serial.print(" ");Serial.print(lrs_walkers[walker][2]);Serial.print(" ");
-  Serial.print(" ");Serial.print(lrs_walkers[walker][3]);Serial.print(" ");
-  Serial.print(" ");Serial.print(lrs_walkers[walker][4]);Serial.print(" ");
-  Serial.print(" ");Serial.print(lrs_walkers[walker][5]);Serial.println(" ");
+      Serial.println("Transistion: Countdown");
 #endif
+      Countdown();
+      break;
+    case 1:
+#ifdef DEBUG
+      Serial.println("Transistion: Red Wipeout");
+#endif
+      WipeOut(matrix.Color(255,0,0),100);  
+      WipeOut(0,100);
+      break;
+    case 2:
+#ifdef DEBUG
+      Serial.println("Transistion: Green Wipeout");
+#endif
+      WipeOut(matrix.Color(0,255,0),100);  
+      WipeOut(0,100);
+      break;
+    case 3:
+#ifdef DEBUG
+      Serial.println("Transistion: Blue Wipeout");
+#endif
+      WipeOut(matrix.Color(0,0,255),100);  
+      WipeOut(0,100);
+      break;
+    case 4:
+#ifdef DEBUG
+      Serial.println("Transistion: Random Double Wipeout");
+#endif
+      WipeOut(matrix.Color(random(255),random(255),random(255)),100);
+      WipeOut(matrix.Color(random(255),random(255),random(255)),75);  
+      WipeOut(0,50); 
+      break;
+    case 5:
+#ifdef DEBUG
+      Serial.println("Transistion: Framer");
+#endif
+      Framer();
+      break;     
+  }
 }
 
 void Countdown() {
@@ -345,5 +281,16 @@ void WipeOut(uint16_t color,int drag) {
         delay(drag);
   #endif
       }
+}
+
+void Framer() {
+  matrix.fillScreen(0);
+  for (int i=255;i>=0;i--) {
+    matrix.drawRect(0,0,w,h,matrix.Color(i,i,i));
+    matrix.show();
+  #ifdef AUDIO
+    playTone(255-(i/2)-25,1);
+  #endif
+  }
 }
 
